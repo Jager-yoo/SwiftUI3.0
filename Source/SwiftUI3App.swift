3d95816d2aff56236d2f18d8dd02981d12950460
Jager-yoo <y73447jh@gmail.com>
2022-05-10 17:24:52 +0900

    file: SwiftUI 프로젝트 추가

---
+++ Source/SwiftUI3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI3App.swift
+//  SwiftUI3
+//
+//  Created by 유재호 on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
